6c720b7bdabbe666867e55c7aaa09f41ba899848
Aleksandr <alexlev83@list.ru>
2023-04-28 08:26:18 +0300

    Команда на ввод элементов массива

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("input a elements for array: " );
+int element = Convert.ToInt32(Console.ReadLine());
+string [] stringArray = new string [element];
+
+// void ShowArray(string [] stringArray)
+// {
+// for (int i = 0; i < stringArray.Length; i++)
+//  Console.Write($"input {i+1} a elements for array: ");
+// }
