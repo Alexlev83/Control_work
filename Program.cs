d4f0e832ec30a9485dca1f5c35ce39846c63e970
Aleksandr <alexlev83@list.ru>
2023-04-28 10:19:24 +0300

    Программа работает

--- Program.cs
+++ Program.cs
@@ -1,18 +1,41 @@
-﻿Console.Write("input a elements for array: " );
+﻿Console.Write("input a quantity elements for array: " );
 int element1 = Convert.ToInt32(Console.ReadLine());
 string [] stringArray = new string [element1];
 
-void ShowArray(string [] stringArray)
+void ShowArray(string[] stringArray)
 {
-for (int i = 0; i < stringArray.Length; i++)
- Console.Write($"input {i+1} a elements for array: ");
- stringArray[i] = Console.ReadLine();
+ for (int i = 0; i < stringArray.Length; i++)
+ {
+  Console.Write($"input {i+1} a elements for array: ");
+   stringArray[i] = Console.ReadLine();
+  }
 }
 string[] element(string[] stringArray)
 {
   int element2 = 0;
-  for (int i = 0; i < stringArray.Length; i++)
- if(stringArray[i].Length <=3)
-  element2++;
+   for (int i = 0; i < stringArray.Length; i++)
+   {
+   if(stringArray[i].Length <=3)
+    element2++;
+   }
+string[] temp = new string[element2];
+int j = 0;
+for (int i = 0; i < stringArray.Length; i++)
+ {
+    if(stringArray[i].Length <=3)
+    {
+     temp[j] = stringArray[i];
+     j++;
+     }
+ }
+     return temp; 
 }
-string[]
+void print(string[] stringArray)
+{
+  Console.Write("[");
+  for (int i = 0; i < stringArray.Length; i++)
+   Console.Write($"{stringArray[i]},"); 
+   Console.Write("]");
+}
+ShowArray(stringArray);
+print(element(stringArray));
