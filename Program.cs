ffeada0373a2cd0a0175530d7e02d3f6c2f47fe6
Aleksandr <alexlev83@list.ru>
2023-04-28 08:35:49 +0300

    Команда на ввод i+1 элемента массива

--- Program.cs
+++ Program.cs
@@ -2,8 +2,9 @@
 int element = Convert.ToInt32(Console.ReadLine());
 string [] stringArray = new string [element];
 
-// void ShowArray(string [] stringArray)
-// {
-// for (int i = 0; i < stringArray.Length; i++)
-//  Console.Write($"input {i+1} a elements for array: ");
-// }
+void ShowArray(string [] stringArray)
+{
+for (int i = 0; i < stringArray.Length; i++)
+ Console.Write($"input {i+1} a elements for array: ");
+ stringArray[i] = Console.ReadLine();
+}
